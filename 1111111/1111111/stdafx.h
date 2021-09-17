8a799e37c23200d7e4e170a8059602d3e1263c61
201510066jodongyeol <gomdol9@naver.com>
2021-09-17 14:42:34 +0900

    Revert "11"
    This reverts commit 84259eb328e4489ab8504e100fe68072aefc5206.

---
+++ 1111111/1111111/stdafx.h
@@ -0,0 +1,13 @@
+//
+//  stdafx.h
+//  1111111
+//
+//  Created by 조동열 on 2021/09/09.
+//
+
+#ifndef stdafx_h
+#define stdafx_h
+#include <stdio.h>
+
+
+#endif /* stdafx_h */
---
+++ practice/practice/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  practice
+//
+//  Created by 조동열 on 2021/09/09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("홍길동\n홍 길 동\n홍  길  동\n");
+    printf("조동열\n");
+    printf("홍은동413-26\n");
+    printf("010-2113-7384\n");
+    printf("c프로그래밍11주차 과제\n");
+    printf("이름:조동열\n");
+    printf("학번:%d\n", 201510066);
+    return 0;
+}
