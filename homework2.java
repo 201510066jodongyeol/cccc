ee1e5494d99d39b81ce3a3ac35c162cca0e2b3d3
201510066jodongyeol <gomdol9@naver.com>
2021-09-30 21:23:53 +0900

    22

---
+++ homework2.java
@@ -0,0 +1,14 @@
+class Student {
+    String name;
+    int codeNumber;
+    String major;
+    String phoneNumber;
+    public void setName(String name ) {
+        this.name=name;
+    }
+    public void setMajor(String major) {
+        System.out.println("");
+    }
+    public void brew() {
+        System.out.println();
+    }
